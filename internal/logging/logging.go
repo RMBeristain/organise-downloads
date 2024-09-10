82783d0ef39935030805451ae98a5afe04d8b7ea
R.M.Beristain <64236102+RMBeristain@users.noreply.github.com>
2024-09-10 12:14:09 +1000

    Replace custom logger with zerolog (#4)
    * Replaces custom logger with zerolog
    
    * Changes log level to debug in tests

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -2,80 +2,47 @@
 package logging
 
 import (
+	"errors"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 
-	"github.com/RMBeristain/organise-downloads/internal/common"
-	"github.com/RMBeristain/organise-downloads/local_utils"
-)
-
-const (
-	LogLevelDebug = iota
-	LogLevelInfo
-	LogLevelWarn
-	LogLevelError
+	"github.com/rs/zerolog"
 )
 
 var (
-	WorkingSrcDir string = "Downloads"
-	LogFileName   string = "organise-downloads.log"
-	LogDirName    string = "log_files"
-	LogLevel      int    = LogLevelInfo
-	Contains             = local_utils.Contains
+	defaultLogParentDir  string = "Downloads"
+	LogFileName          string = "organise-downloads.log"
+	LogDir               string = "log_files"
+	ConfiguredZerologger zerolog.Logger
 )
 
-type Logr struct {
-	logDebug *log.Logger
-	LogInfo  *log.Logger
-	LogError *log.Logger
-	LogFatal *log.Logger
+type Zerologger struct {
+	zerolog.Logger
 }
 
-// log_debug logs a DEBUG level message if LogLevel is set to `LogLevelDebug`
-func LogDebug(logr *Logr, message string, args ...interface{}) {
-	if LogLevel == LogLevelDebug {
-		defer func() {
-			if r := recover(); r != nil {
-				// LogDebug isn't defined yet
-				log.Printf(message, args...)
-			}
-		}()
-		logr.logDebug.Printf(message, args...)
-	}
-}
+// InitZeroLog configures a zerolog Logger and returns an instance of the Zerologr struct that contains it.
+func InitZeroLog() Zerologger {
+	userHomeDir, _ := os.UserHomeDir()
+	logDirPath := filepath.Join(userHomeDir, defaultLogParentDir, LogDir)
 
-// InitLoggingToFile creates or prepares a file for logging and returns the address of a configured Logr struct.
-func InitLoggingToFile(WorkingSrcDir *string, pNewLogLevel *int) *Logr {
-	var logDir string
-	var logFile string
-	var logr Logr
-
-	if *pNewLogLevel != LogLevel && LogLevelDebug <= *pNewLogLevel && *pNewLogLevel <= LogLevelError {
-		LogLevel = *pNewLogLevel
+	// check if logDirPath exists; create it if not.
+	_, err := os.Stat(logDirPath)
+	if errors.Is(err, fs.ErrNotExist) {
+		err = os.Mkdir(logDirPath, 0777)
+		if err != nil {
+			log.Fatalf("unable to create logging dir %v", logDirPath)
+		}
 	}
 
-	logDir = filepath.Join(*WorkingSrcDir, LogDirName)
+	logFilePath := filepath.Join(logDirPath, LogFileName)
 
-	if created, err := common.CreateDirIfNotExists(logDir); err != nil {
-		log.Fatalf("FATAL: Unable to create dir %v - %v", logDir, err)
-	} else if created {
-		log.Printf("Created missing directory %v", logDir)
-	}
-
-	logFile = filepath.Join(logDir, LogFileName)
-
-	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Panicf("Cannot write to log file %v - %v", logFile, err)
+		log.Panicf("Cannot write to log file %v - %v", logFilePath, err)
 	}
 
-	logr.logDebug = log.New(file, "DEBUG: ", log.Ldate|log.Ltime|log.Llongfile)
-	logr.LogInfo = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	logr.LogError = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-	logr.LogFatal = log.New(file, "FATAL: ", log.Ldate|log.Ltime|log.Llongfile)
-
-	LogDebug(&logr, "Writing log to %v", logDir)
-
-	return &logr
+	ConfiguredZerologger = zerolog.New(file).With().Timestamp().Caller().Logger()
+	return Zerologger{ConfiguredZerologger}
 }
--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/RMBeristain/organise-downloads/internal/common"
 	"github.com/RMBeristain/organise-downloads/internal/logging"
 	"github.com/RMBeristain/organise-downloads/internal/org"
+	"github.com/rs/zerolog"
 )
 
 var (
@@ -18,38 +20,47 @@ func main() {
 	var pWorkingSrcDir *string
 
 	start_time := time.Now()
+
 	pDownloadDir := flag.String("downloads", defaultSrcDir, "Full path to Downloads dir")
-	pNewLogLevel := flag.Int("loglevel", logging.LogLevelInfo, "Use this log level [0:3]")
+	pNewLogLevel := flag.Int("loglevel", int(zerolog.InfoLevel), "Use this log level [0:3]")
 	flag.Parse() // read command line flags
 
+	if int(zerolog.TraceLevel) <= *pNewLogLevel && *pNewLogLevel <= int(zerolog.PanicLevel) {
+		zerolog.SetGlobalLevel(zerolog.Level(*pNewLogLevel))
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+	logger := logging.InitZeroLog()
+	logger.Trace().Int("GlobalLogLevel", *pNewLogLevel).Msg("set new log level")
+
 	if *pDownloadDir != defaultSrcDir {
+		logger.Debug().Str("downloadDir", *pDownloadDir).Msg("changed source dir")
 		pWorkingSrcDir = pDownloadDir // use command line value
 	} else {
 		pWorkingSrcDir = common.GetCurrentUserDownloadPath(defaultSrcDir)
 	}
 
-	logr := *logging.InitLoggingToFile(pWorkingSrcDir, pNewLogLevel)
-	logr.LogInfo.Print("START.")
+	logger.Info().Msg("START.")
 	files, err := os.ReadDir(*pWorkingSrcDir) // get all files
 
 	if err != nil {
-		logr.LogFatal.Fatalf("FATAL: %v", err)
+		logger.Fatal().Err(err).Msg(err.Error())
 	}
 
 	filesChannel := make(chan string, 4)
 	filesToMove := org.GetFilesToMove(files, excludedExtensions())
 
 	if len(filesToMove) > 0 {
-		logr.LogInfo.Printf("Files to move : %v\n", filesToMove)
-		go org.MoveFiles(*pWorkingSrcDir, filesToMove, &logr, filesChannel)
+		logger.Debug().Str("filesToMove", fmt.Sprintf("%v", filesToMove))
+		go org.MoveFiles(*pWorkingSrcDir, filesToMove, filesChannel)
 		for fileMoved := range filesChannel {
-			logr.LogInfo.Printf("...moved %v", fileMoved)
+			logger.Info().Str("filePath", fileMoved).Msg("new location")
 		}
 	} else {
-		logr.LogInfo.Print("No files to move.")
+		logger.Info().Msg("No files to move.")
 	}
 
-	logr.LogInfo.Printf("(%v) DONE.", time.Since(start_time))
+	logger.Info().Dur("elapsedTime", time.Since(start_time)).Msg("DONE.")
 	os.Exit(0)
 }
 
